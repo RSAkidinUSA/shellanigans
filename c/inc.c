d3b073615018fb0dc23a3b37781c3ee56d8c13e3
Ryan Burrow <rsardb11@vt.edu>
2018-02-21 22:53:17 -0500

    Added *extensive* error checking

--- c/inc.c
+++ c/inc.c
@@ -1,8 +1,8 @@
-int printf(  const      char   * format,     ...  );;      int
-atoi(const   char *     str)  ;void INC(    int    *a)    {(*a)
-    ++;      };;;;;;    ;;;;  ;;;;        ;;;        ;;;   ;;; 
-    int      main(int   c ,   int*        *v          ){      
-    {;}      ;int  i =  c>1?  atoi        (v          [1   ]):
-    -1;      INC(   &i) ;{;}  {};;        ;{{        };}  ;;;;;
-printf("ar"  "gv"    "[1] +"  " 1 = %d      \n"    ,i)     ;;; 
-return i-1;  };;;     ;;;;;;   ;;;;;;;;;     ;;;  ;;;     ;;   
+int printf(  const      char   * format,     ...    );;     int
+atoi(const   char *     str)  ;void INC (   int      *a)   {(*a)
+    ++;      };;;;;;    ;;;;  ;;;;         ;;;        ;;;   ;;; 
+    int      main(int   c ,   int*         *v          ){      
+    {;}      ;int  i =  c>1?  atoi         (v          [1   ]):
+    0;;      if(i   )INC(&i)  ;if(         i){        ;;;  ;;;;;
+printf("%s"  " +"    " 1 = "  "%d\n",v[1]   ,i);     ;;;    ;;}    
+else printf  ("E"    "nter "   "an int>0      \n"   );}    ;;
