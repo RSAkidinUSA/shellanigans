f6dfd8906703a0ddedc6c2aea78f0c58bf91f30e
Ryan Burrow <rsardb11@vt.edu>
2018-02-21 19:05:28 -0500

    Added first version of inc.c, updates to come

---
+++ c/inc.c
@@ -0,0 +1,8 @@
+int printf(  const      char   * format,     ...  );;      int
+atoi(const   char *     str)  ;void INC(    int    *a)    {(*a)
+    ++;      };;;;;;    ;;;;  ;;;;        ;;;        ;;;   ;;; 
+    int      main(int   c ,   int*        *v          ){      
+    {;}      ;int  i =  c>1?  atoi        (v          [1   ]):
+    -1;      INC(   &i) ;{;}  {};;        ;{{        };}  ;;;;;
+printf("ar"  "gv"    "[1] +"  " 1 = %d      \n"    ,i)     ;;; 
+return i-1;  };;;     ;;;;;;   ;;;;;;;;;     ;;;  ;;;     ;;   
