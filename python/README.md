9125912215597e3e4821a5bb1780d6723fe4f001
Ryan Burrow <rsardb11@vt.edu>
2018-02-23 09:12:41 -0500

    Added basic command line options, updated README.md

--- python/README.md
+++ python/README.md
@@ -2,11 +2,23 @@
 This directory contains various python scripts to make your life easier
 
 Makefile:
-	Use "make install-(script)" to install a specific script
-	Use "make install" to install all scripts
-	Check the script section in this readme to see more about the script
 
-cryptocheck.py
-	Checks the current price of bitcoin for you, using a similar strategy to that of [bogosort](https://en.wikipedia.org/wiki/Bogosort)
+- Use "make install-(script)" to install a specific script
+- Use "make install" to install all scripts
+- Check the script section in this readme to see more about the script
 
-		
+cryptocheck.py:
+Checks the current price of bitcoin for you, using a similar strategy to that of [bogosort](https://en.wikipedia.org/wiki/Bogosort)
+
+```
+usage: cryptocheck.py [-h] [-c {BTC,LTC,ETH,BCH}] [-r {EU,UK,US}]
+
+Determine crypto currency exchange rate following a bogosort approach!
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -c {BTC,LTC,ETH,BCH}, --crypto {BTC,LTC,ETH,BCH}
+                        Crypto currency to be exchanged
+  -r {EU,UK,US}, --region {EU,UK,US}
+                    	Region of exchange
+```	
--- python/cryptocheck/cryptocheck.py
+++ python/cryptocheck/cryptocheck.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
+#coding: <utf-8>
+
+# A python script for (possibly) determining the current price of a crypto
+# currency, following a similar approach to bogosort
+import argparse
 import requests
 import random
 import time
 import sys
 
 API_URL = "https://api.gdax.com"
-crypto_list = ['BTC', 'LTC', 'ETH', 'BTH']
+crypto_list = ['BTC', 'LTC', 'ETH', 'BCH']
 region_dict = { 'US' : {'symbol' : '$', 'string' : 'USD'}, \
-				'UK' : {'symbol' : '£', 'string' : 'GPB'}, \
+				'UK' : {'symbol' : '£', 'string' : 'GBP'}, \
 				'EU' : {'symbol' : '€', 'string' : 'EUR'} }
 
 def _get_price(crypto='BTC', region='US'):
@@ -17,8 +22,12 @@ def _get_price(crypto='BTC', region='US'):
 	
 	r = requests.get(API_URL + '/products/' + exchange_symbol + '/book')
 	data = r.json()
+	try:
+		retVal = (float(data['bids'][0][0]), exchange_symbol, region_symbol)
+		return retVal
+	except:
+		return (None, exchange_symbol, region_symbol)
 
-	return (float(data['bids'][0][0]), exchange_symbol, region_symbol)
 
 def _guess_price():
 	guess_dollar = random.randint(0,20000)
@@ -26,7 +35,19 @@ def _guess_price():
 
 	return float(guess_dollar) + (float(guess_cents) / 100.00)
 
+def _parse_args():
+	parser = argparse.ArgumentParser(description='Determine crypto currency exchange rate '\
+									'following a bogosort approach!')
+	parser.add_argument('-c', '--crypto', choices=crypto_list, default='BTC',
+	                    help='Crypto currency to be exchanged')
+	parser.add_argument('-r', '--region', choices=region_dict.keys(), default='US',
+	                    help='Region of exchange')
+
+	return parser.parse_args()
+
 def __main__():
+
+	args = _parse_args()
 
 	# seed a guess
 	random.seed(int(time.time() * 100))
@@ -35,10 +56,13 @@ def __main__():
 	guess = _guess_price()
 
 	# get price as well as exchange_symbol and region_symbol for given currency and region
-	price, exchange_symbol, region_symbol = _get_price()
+	price, exchange_symbol, region_symbol = _get_price(region=args.region, crypto=args.crypto)
 
-	print("The current exchange of %s is%s: %s%.2f" % \
-		(exchange_symbol, "" if guess == price else " not", region_symbol, guess))
+	if (price != None):
+		print("The current exchange of %s is%s: %s%.2f" % \
+			(exchange_symbol, "" if guess == price else " not", region_symbol, guess))
+	else:
+		print("That combination is not currently available for comparison")
 
 if __name__ == '__main__':
 	__main__()
