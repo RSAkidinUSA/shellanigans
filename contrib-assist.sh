edf216b9a4d11c31ce8a226e7b6ea0fb2cd74d51
Ryan Burrow <rsardb11@vt.edu>
2018-02-24 21:25:13 -0500

    List current program types and ask user what type they would like to make

--- contrib-assist.sh
+++ contrib-assist.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Check if user has Github ssh access
-ssh -T git@github.com &> .ssh-auth
+ssh -T git@github.com &> .ssh_auth
 if [[ $? == 255 ]]; then
 	rm .ssh-auth
 	echo "You will need to set up ssh keys on Github to use this script"
@@ -10,23 +10,40 @@ if [[ $? == 255 ]]; then
 	echo "Then https://help.github.com/articles/adding-a-new-ssh-key-to-your-github-account/"
 	echo "Finally https://help.github.com/articles/testing-your-ssh-connection/"
 else
-	ID=$(cat .ssh-auth | cut -f2 -d' ' | cut -f1 -d'!')
+	ID=$(cat .ssh_auth | cut -f2 -d' ' | cut -f1 -d'!')
 	COLLAB=false
-	rm .ssh-auth
+	rm .ssh_auth
 	for i in $(cat .collaborators); do
 		if [[ ${ID,,} == ${i,,} ]]; then
 			COLLAB=true
 			break
 		fi
 	done
+	# Get the the type of program
+	echo "Please enter the type of program you would like to add"
+	echo "Existing options are:"
+	for existing in $(find . -maxdepth 1 -type d | cut -d / -f 2 | grep -v '\.'); do
+		echo '-' $existing
+	done
+	read program_type
+	newProg=true
+	for existing in $(find . -maxdepth 1 -type d | cut -d / -f 2 | grep -v '\.'); do
+		if [ "$existing" == "$program_type" ]; then
+			newProg=false
+			break
+		fi
+	done
+	if [[ $newProg == true ]]; then
+		echo "Make new directory for" $program_type "programs?"
+		read resp
+	fi
+	# Get the name of the program
+
 	if [[ $COLLAB == true ]]; then
 		# do a branch
-		echo $ID
+		echo $program_type
 	else
 		# do a fork
-		echo $ID|rev
+		echo $program_type|rev
 	fi
 fi
-
-# Get github password
-# Authenticate to github
