58e2464dcac9725dcdca2db259eca1389635c8bb
Ryan Burrow <rsardb11@vt.edu>
2018-02-26 18:55:45 -0500

    Initial commit for color-bomb

---
+++ python/color-bomb/color-bomb.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+RED_TEXT 	= "\x1b[1;31;40m"
+GREEN_TEXT	= "\x1b[1;32;40m"
+
+def main():
+	# setup to launch in background
+	print("Hello world!")
+	# setup random delay
+	# 
+
+if __name__ == '__main__':
+	main()
